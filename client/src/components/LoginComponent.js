d6cabf1aadbcd0a1bd7de14fccf1b929573bbcc1
ThanapatWongtungjaroensuk <91516576+ThanapatWongtungjaroensuk@users.noreply.github.com>
2022-02-25 23:27:29 +0700

    Login/Regis V1

--- client/src/components/LoginComponent.js
+++ client/src/components/LoginComponent.js
@@ -5,8 +5,31 @@ const LoginComponent=()=>{
         <div>   
             <NavbarComponent/>
             <div className="container"> 
-                <h1>Login Page</h1>
-                <h1>Hello</h1>
+                <form>
+                    <h3>Login</h3>
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input type="email" className="form-control" placeholder="Enter email" />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input type="password" className="form-control" placeholder="Enter password" />
+                    </div>
+
+                    <div className="form-group">
+                        <div className="custom-control custom-checkbox">
+                            <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                            <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                        </div>
+                    </div>
+
+                    <p className="forgot-password text-right">
+                        Forgot <a href="#">password?</a>
+                    </p>
+                    
+                    <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
+                </form>
             </div>
         </div>
     )
--- client/src/components/RegisterComponent.js
+++ client/src/components/RegisterComponent.js
@@ -4,7 +4,35 @@ const RegisterComponent=()=>{
         <div>
             <NavbarComponent/>
             <div className="container"> 
-                <h1>Register</h1>
+                <form>
+                    <h3>Register</h3>
+
+                    <div className="form-group">
+                        <label>First name</label>
+                        <input type="text" className="form-control" placeholder="First name" />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Last name</label>
+                        <input type="text" className="form-control" placeholder="Last name" />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input type="email" className="form-control" placeholder="Enter email" />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input type="password" className="form-control" placeholder="Enter password" />
+                    </div>
+
+                    <p className="forgot-password text-right">
+                        Already registered <a href="#">log in?</a>
+                    </p>
+
+                    <button type="submit" className="btn btn-dark btn-lg btn-block">Register</button>
+                </form>
             </div>
         </div>
     )
