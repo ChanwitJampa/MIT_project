6056664dc04dfb51c7e70baa80453faebbb4116e
ThanapatWongtungjaroensuk <91516576+ThanapatWongtungjaroensuk@users.noreply.github.com>
2022-02-26 00:33:39 +0700

    Login/Regis V2

--- client/src/components/LoginComponent.js
+++ client/src/components/LoginComponent.js
@@ -1,35 +1,33 @@
 import { useEffect, useState } from "react";
 import NavbarComponent from "./NavbarComponent";
+import "./LoginComponent.css";
 const LoginComponent=()=>{
     return(
         <div>   
             <NavbarComponent/>
-            <div className="container"> 
-                <form>
-                    <h3>Login</h3>
-                    <div className="form-group">
-                        <label>Email</label>
-                        <input type="email" className="form-control" placeholder="Enter email" />
-                    </div>
+            <div className="container">
+                <div className="outer">
+                    <div className="inner">
+                        <form>
+                            <h3>Login</h3>
+                            <div className="form-group">
+                                <label>Email</label>
+                                <input type="email" className="form-control" placeholder="Enter email" />
+                            </div>
 
-                    <div className="form-group">
-                        <label>Password</label>
-                        <input type="password" className="form-control" placeholder="Enter password" />
-                    </div>
+                            <div className="form-group">
+                                <label>Password</label>
+                                <input type="password" className="form-control" placeholder="Enter password" />
+                            </div>
 
-                    <div className="form-group">
-                        <div className="custom-control custom-checkbox">
-                            <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                            <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                        </div>
+                            <p className="forgot-password text-right">
+                                Forgot <a href="#">password?</a>
+                            </p>
+                            
+                            <button type="submit" className="btn btn-danger">Sign in</button>
+                        </form>
                     </div>
-
-                    <p className="forgot-password text-right">
-                        Forgot <a href="#">password?</a>
-                    </p>
-                    
-                    <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
-                </form>
+                </div>
             </div>
         </div>
     )
--- client/src/components/RegisterComponent.js
+++ client/src/components/RegisterComponent.js
@@ -1,38 +1,54 @@
 import NavbarComponent from "./NavbarComponent";
+import "./RegisterComponent.css";
 const RegisterComponent=()=>{
     return(
         <div>
             <NavbarComponent/>
-            <div className="container"> 
-                <form>
-                    <h3>Register</h3>
+            <div className="container">
+                <div className="outer">
+                    <div className="inner">
+                        <form>
+                            <h3>Register</h3>
 
-                    <div className="form-group">
-                        <label>First name</label>
-                        <input type="text" className="form-control" placeholder="First name" />
+                            <div className="form-group">
+                                <label>First name</label>
+                                <input type="text" className="form-control" placeholder="First name" />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Last name</label>
+                                <input type="text" className="form-control" placeholder="Last name" />
+                            </div>
+
+                            <div className="form-group">
+                                <label>ID card</label>
+                                <input type="text" className="form-control" placeholder="XXXXX-XXXXX-XXX" />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Email</label>
+                                <input type="email" className="form-control" placeholder="Enter email" />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Password</label>
+                                <input type="password" className="form-control" placeholder="Enter password" />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Repeat Password</label>
+                                <input type="password" className="form-control" placeholder="Enter password" />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Hospital</label>
+                                <input type="password" className="form-control" placeholder="Enter password" />
+                            </div>
+
+                            <button type="submit" className="btn btn-danger">Sign up</button>
+                        </form>
                     </div>
-
-                    <div className="form-group">
-                        <label>Last name</label>
-                        <input type="text" className="form-control" placeholder="Last name" />
-                    </div>
-
-                    <div className="form-group">
-                        <label>Email</label>
-                        <input type="email" className="form-control" placeholder="Enter email" />
-                    </div>
-
-                    <div className="form-group">
-                        <label>Password</label>
-                        <input type="password" className="form-control" placeholder="Enter password" />
-                    </div>
-
-                    <p className="forgot-password text-right">
-                        Already registered <a href="#">log in?</a>
-                    </p>
-
-                    <button type="submit" className="btn btn-dark btn-lg btn-block">Register</button>
-                </form>
+                </div>
             </div>
         </div>
     )
