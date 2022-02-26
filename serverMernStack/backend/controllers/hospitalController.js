57ceb5dc86ad680cfb1bd85aaccfd42f7529d9f6
Chanwit Jampa <chanwit.ja@ku.th>
2022-02-26 23:29:10 +0700

    backend version1.0
    update delete get getbyid hospital
    path "/api/hospitals"

---
+++ serverMernStack/backend/controllers/hospitalController.js
@@ -0,0 +1,87 @@
+
+const asyncHandler = require('express-async-handler')
+
+const Hospital = require('../models/hospitalModel')
+
+//@desc Get Hospitals
+//@route GET /api/hospitals
+//@access Private
+const getHospitals = asyncHandler(async (req, res) => {
+    const hospitals = await Hospital.find()
+    res.status(200).json(hospitals)
+})
+
+//@desc Set Hospital
+//@route POST /api/hospitals
+//@access Private
+const setHospital = asyncHandler(async (req, res) => {
+    if (!req.body.name) {
+        res.status(400)
+        throw new Error('Please add a name field')
+    }
+    if (!req.body.latitude) {
+        res.status(400)
+        throw new Error('Please add a latitude field')
+    }
+    if (!req.body.longitude) {
+        res.status(400)
+        throw new Error('Please add a longitude field')
+    }
+
+    const hospital = await Hospital.create({
+        name: req.body.name,
+        latitude: req.body.latitude,
+        longitude: req.body.longitude
+    })
+    res.status(200).json(hospital)
+
+})
+
+
+//@desc Update Hospital
+//@route PUT /api/hospitals/:id
+//@access Private
+const putHospital = asyncHandler(async (req, res) => {
+    const hospital = await Hospital.findById(req.params.id)
+    if(!hospital){
+        res.status(400)
+        throw new Error('hospital id not found')
+    }
+    const updatedHospital = await Hospital.findByIdAndUpdate(req.params.id,req.body,{new:true})
+    res.status(200).json(updatedHospital)
+})
+
+//@desc Delete Hospital
+//@route DELETE /api/hospitals/:id
+//@access Private
+const deleteHospital = asyncHandler(async (req, res) => {
+    const hospital = await Hospital.findById(req.params.id)
+    if(!hospital){
+        res.status(400)
+        throw new Error('hospital id not found')
+    }
+    const deleteHospital = await Hospital.findByIdAndDelete(req.params.id)
+    res.status(200).json({id:req.params.id})
+})
+
+
+//@desc Get Hospital
+//@route GET /api/hospitals/:id
+//@access Private
+const getHospital = asyncHandler(async(req,res)=>{
+    const hospital = await Hospital.findById(req.params.id)
+    if(!hospital){
+        res.status(400)
+        throw new Error('hospital id not found')
+    }
+    res.status(200).json(hospital)
+})
+
+
+module.exports = {
+    getHospitals,
+    setHospital,
+    putHospital,
+    deleteHospital,
+    getHospital
+}
