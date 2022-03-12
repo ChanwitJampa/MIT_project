89afc1c9908ccefe8c63aeaa3f623c54b7324804
ThanapatWongtungjaroensuk <91516576+ThanapatWongtungjaroensuk@users.noreply.github.com>
2022-03-12 23:08:39 +0700

    Update RegisterComponent.js

--- client/src/components/RegisterComponent.js
+++ client/src/components/RegisterComponent.js
@@ -4,15 +4,15 @@ import "./RegisterComponent.css";
 import axios from "axios";
 import Swal from "sweetalert2";
 const RegisterComponent=()=>{
-    const [hospital,setHospital]=useState([])
+    const [hospital,setHospital]=useState([]);
+    const [hospitalID, setHospitalID] = useState("");
+    const [hospitalName, setHospitalName] = useState("");
     const [user,setUser]=useState({
         firstName:"",
         lastName:"",
         idCard:"",
         email:"",
         password:"",
-        hospitalName:"",
-        hospitalID:"",
     })
     const fetchData=()=>{
         axios.get(`http://localhost:5000/api/hospitals`).then(res=>{
@@ -26,12 +26,35 @@ const RegisterComponent=()=>{
         fetchData()
     },[])
 
-    const {firstName,lastName,idCard,email,password,hospitalName,hospitalID}=user
+    const inputHospital=(id)=>{
+        console.log(id);
+        const Name = hospital.filter((hospital)=>{
+            if(hospital._id===id){
+                return hospital
+            }
+        }).map((hospital)=>{
+            return hospital.hospitalName
+        })
+        setHospitalName(Name.toString());
+        setHospitalID(id);
+    }
+
+    const {firstName,lastName,idCard,email,password}=user
+    
     const inputValue = (name) => (event) =>{
         setUser({...user,[name]:event.target.value});
     }
     const signupForm=(event)=>{
         event.preventDefault();
+        console.table({
+            firstName,
+            lastName,
+            idCard,
+            email,
+            password,
+            hospitalID,
+            hospitalName
+        });
         axios.post(`http://localhost:5000/api/user`,{firstName,lastName,idCard,email,password,hospitalName,hospitalID}).then(res=>{
             console.log(res.data)
             setUser({...user,
@@ -40,8 +63,8 @@ const RegisterComponent=()=>{
             idCard:"",
             email:"",
             password:"",
-            hospitalName: hospital[0].hospitalName,
-            hospitalID: hospital[0]._id})
+            hospitalName: "",
+            hospitalID: ""});
 
             Swal.fire("ลงทะเบียนสำเร็จ", "บันทึกข้อมูลเรียบร้อย", "success");
         })
@@ -95,10 +118,7 @@ const RegisterComponent=()=>{
 
                             <div className="form-group">
                                 <label>Hospital</label>
-                                <select  aria-label="Default select example" 
-                                    onChange={(event) => {
-                                    setUser(event.target.value);
-                                    }}>
+                                <select  aria-label="Default select example" onChange={(event)=>inputHospital(event.target.value)}>
                                     <option selected disabled>Hospital</option>
                                     {hospital.map((hospital) => (
                                     <option value={hospital._id}>{hospital.hospitalName}</option>
